3646284da32e9ee45d270c1525e8b2aa79e50732
gurusmart555@gmail.com <gurusmart555@gmail.com>
2025-02-21 04:05:26 +0530

    rewrite prop management
    Shifted prop management from server to client, utilizing local entities

--- config/shared.lua
+++ config/shared.lua
@@ -1,19 +1,19 @@
 return {
     bridges = {
         {
-            hash = 3685260158,
+            hash = `car_drawbridge`,
             normalState = vec3(353.3317, -2315.838, 13.75),
             openState = vec3(353.3317, -2315.838, 42.00)
         },
         {
-            hash = 4287851603,
+            hash = `train_drawbridge`,
             normalState = vec3(219.5085, -2319.432, 13.25),
             openState = vec3(219.5085, -2319.432, 42.00)
         }
     },
     blockArea = {
         {
-            coords = vec3(347.376, -2279.668, 10.184),
+            coords = vec3(347.7381, -2273.5859, 10.1941), -- 353.3167, -2316.1406, 10.2630,
             size = 10.0,
         },
         {
@@ -30,39 +30,43 @@ return {
         rotation = 0.0,
         model = `prop_elecbox_01a`,
         cooldown = 60 * 60000, -- 60 minutes
+        minigame = function ()
+            return true            
+        end
     },
     barrierGates = {
         {
             model = `prop_bridge_barrier_gate_01x`,
-            coords = vec3(364.69915771484377, -2343.686279296875, 11.39140605926513),
-            normalRot = vec3(0.000000, -0.000000, -0.000010),
-            minRotationY = 90,
+            coords = vec3(364.69, -2343.68, 11.39),
+            normalRot = vec3(0.0, 0.0, 0.0),
+            minRotationY = 0,
             maxRotationY = -90,
             rotationX = 180
         },
         {
             model = `prop_bridge_barrier_gate_01x`,
-            coords = vec3(342.0478515625, -2343.5517578125, 11.46424674987793),
-            normalRot = vec3(0.000000, -0.000000, 179.999985),
-            minRotationY = -90,
+            coords = vec3(342.04, -2343.55, 11.46),
+            normalRot = vec3(0.0, 0.0, 180.0),
+            minRotationY = 0,
             maxRotationY = 90,
             rotationX = 0
         },
+
         {
             model = `prop_bridge_barrier_gate_01x`,
-            coords = vec3(364.7115478515625, -2288.304443359375, 11.36281585693359),
-            normalRot = vec3(0.000000, -0.000000, -0.000010),
-            minRotationY = 90,
+            coords = vec3(364.71, -2288.3, 11.36),
+            normalRot = vec3(0.0, 0.0, 0.0),
+            minRotationY = 0,
             maxRotationY = -90,
-            rotationX = 180
+            rotationX = 0
         },
         {
             model = `prop_bridge_barrier_gate_01x`,
-            coords = vec3(342.1158447265625, -2288.013671875, 11.30535125732421),
-            normalRot = vec3(0.000000, -0.000000, 179.999985),
-            minRotationY = -90,
+            coords = vec3(342.11, -2288.01, 11.30),
+            normalRot = vec3(0.0, 0.0, 180.0),
+            minRotationY = 0,
             maxRotationY = 90,
-            rotationX = 0
+            rotationX = -180
         }
     }
 }
